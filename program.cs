86dfa1b75714d172f01587c1905af0eedfe537c8
朱帅 <zhushuai@zhushuaideMacBook-Pro.local>
2018-12-05 23:08:03 +0800

    远坂凛

---
+++ program.cs
@@ -0,0 +1,10 @@
+using System;
+delegate double function(double x);
+class Program{
+double factor
+    static void Main(string[] args){
+         Console.WriteLine("hellow world");
+         
+         
+    }
+}
